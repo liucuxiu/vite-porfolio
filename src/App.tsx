72681f59395a288af8583f8d34a34db4285d4fec
liucuxiu <xliucuxiux@gmail.com>
2023-06-20 10:44:16 +0700

    Build first layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import { Button } from '@chakra-ui/react';
+import { Grid, GridItem } from '@chakra-ui/react';
 
 function App() {
   return (
-    <Button>Hehe</Button>
+    <Grid
+      templateAreas={`"nav main"`}
+      gridTemplateColumns={'500px 1fr'}
+      gap='1'
+      color='blackAlpha.700'
+      fontWeight='bold'
+    >
+      <GridItem pl='2' bg='pink.300' area={'nav'}>
+        Nav
+      </GridItem>
+      <GridItem pl='2' bg='green.300' area={'main'}>
+        Main
+      </GridItem>
+    </Grid>
   )
 }
 
